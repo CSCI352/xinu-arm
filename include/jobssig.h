df92e408d6dcba8ee535da49cb8f3a8de7622c10
CODY MATTHEW HOLLEY <cmh6807@cs.rit.edu>
2013-11-19 23:41:37 -0500

    Added header files

---
+++ include/jobssig.h
@@ -0,0 +1,9 @@
+/**
+ * File: jobssig.h
+ * Author:
+ * Contributors: 
+ * Description: (change this later)
+ *	Have handlers for foreground/background processes that
+ *	will do the appropriate functionality for any signals generated
+ *	Also implement handling for suspended killed processes
+ */
---
+++ include/jobssuspend.h
@@ -0,0 +1,9 @@
+/**
+ * File: jobssuspend.h
+ * Author:
+ * Contributors: 
+ * Description: (change this later)
+ *	This should be linked into signal handling. There are
+ *	a couple of different functionalities that need to be implemented,
+ *	especially if we support being able to suspend things with signals
+ */
