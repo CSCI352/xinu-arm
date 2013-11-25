009cb229fc49c2e0006722244540f0e2b6460f79
REBECCA BANAFSHEH VESSAL <rbv7405@cs.rit.edu>
2013-11-24 22:09:52 -0500

    Fixed ps command and added skeleton for grouping processes into jobs and having a job list

--- include/jobsgroup.h
+++ include/jobsgroup.h
@@ -1,7 +1,30 @@
 /**
  * File: jobsgroup.h
- * Author:
+ * Author: Rebecca Vessal
  * Contributors: 
  * Description: (change this later)
  *	Linked list for processes and use the parent process as the group ID
  */
+ 
+ class GroupThreads
+ {
+public:
+	//Struct for job that is implemented like a linked list
+	typedef struct{
+		struct thrent *headThread;//Pointer to thread that is first thread to be added to the job
+		struct thrent *tailThread;//Pointer to the thread that is last thread to be added to the job
+	}Job
+	//Struct for allowing a thread to have groupID and a parentThread 
+	//as it is being pulled out of the threadTable
+	typedef struct{
+		tid_typ groupID;			//This groupID corresponds to the parent process ID.
+									//thrent struct uses tid_type as its PPID AKA Parent process ID.  
+									//See:thrent in Xinu shell.
+		struct thrent *parentThread;//Pointer to thread that is parent to the current one
+		struct thrent *childThread;//Pointer to a child thread if this thread has a child thread
+	}Process
+	//List of jobs
+	struct Job *listOfJobs;
+	//Get each thread from the thread table and put them into jobs
+	void generateJob();
+ }
--- shell/xsh_jobsgroup.c
+++ shell/xsh_jobsgroup.c
@@ -1,9 +1,20 @@
 /**
  * File: xsh_jobsgroup.c
- * Author:
+ * Author: Rebecca Vessal
  * Contributors:
  * Description: (change this later)
  *      Linked list for processes and use the parent process as the group ID
  */
 
 #include <jobsgroup.h>
+//Get each thread from the thread table and put them into jobs
+void generateJob()
+{
+    while((thrptr = &thrtab[i]) != NULL)
+    {
+        if (thrptr->state == THRFREE)
+        {
+            continue;
+        }
+    }
+}
