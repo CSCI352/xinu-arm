45b17ac1f165801bc0173498b396c3dbc0d4df31
bitbanger <lxl5734@rit.edu>
2013-11-30 16:55:44 -0500

    Add in (but do not use) PTY functions to abstract ptys from shell

---
+++ include/pty.h
@@ -0,0 +1,22 @@
+/**
+ * @file pty.h
+ *
+ * Provides a pseudo-terminal interface meant mainly for XINU shells.
+ * Ideally, this would be a pseudo-device proper.
+ * As it stands, it's a series of kernel calls to abstract the multiplexing logic away from the shells.
+ */
+/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+
+#ifndef _PTY_H_
+#define _PTY_H_
+
+#include <tty.h>
+
+/* Pseudo-TTY functions */
+void ptyRead(int, void *, uint);
+void ptyPutc(int, char);
+char ptyGetc(int);
+void ptyPrintf(int, const char *, ...);
+void ptyControl(int, int, long, long);
+
+#endif                          /* _PTY_H_ */
---
+++ system/pty.c
@@ -0,0 +1,53 @@
+/**
+ * @file pty.c
+ *
+ * Implements the pseudo-tty functions described in pty.h
+ */
+/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+
+#include <pty.h>
+
+static int activePtyId = 1;
+
+void ptyRead(int ptyId, void *buf, uint buflen) {
+	/* Wait for the pty to become active */
+	while(ptyId != activePtyId);
+	
+	/* Read from stdin into the buffer */
+	read(stdin, buf, buflen);
+}
+
+void ptyPutc(int ptyId, char ch) {
+	/* Wait for the pty to become active */
+	while(ptyId != activePtyId);
+	
+	/* Put a character */
+	putc(stdout, ch);
+}
+
+char ptyGetc(int ptyId) {
+	/* Wait for the pty to become active */
+	while(ptyId != activePtyId);
+	
+	return getc(stdin);
+}
+
+void ptyPrintf(int ptyId, const char *fmt, ...) {
+	/* Wait for the pty to become active */
+	while(ptyId != activePtyId);
+	
+	va_list ap;
+    
+    va_start(ap, fmt);
+    _doprnt((char *)fmt, ap, putc, stdout);
+    va_end(ap);
+    
+    return 0;
+}
+
+void ptyControl(int ptyId, int func, long arg1, long arg2) {
+	/* Wait for the pty to become active */
+	while(ptyId != activePtyId);
+	
+	control(stdin, func, arg1, arg2);
+}
