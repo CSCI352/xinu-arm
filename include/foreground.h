85c07002b0eb530dd650970f03b194bcfc33173f
Cody Holley <cmh6807@cs.rit.edu>
2013-12-07 21:06:01 -0500

    Cleaned up code documentation

--- include/foreground.h
+++ include/foreground.h
@@ -2,11 +2,9 @@
  * File: foreground.h
  * Author: Cody Holley (cmh6807)
  * Contributors: Marit Brocker (mlb8494)
- * Description: (change this later)
- *	(Foreground/Background processes)
- *	Implement shifting processes between levels.
- *	This stage will include testing to make sure signals are
- *	handled correctly, no matter which direction the processes switch
+ * Description:
+ *	Moves a job from whatever state it may currently be in 
+ *	to the foreground.
  */
 
 #ifndef _FOREGROUND_H_
@@ -19,8 +17,8 @@
 #include <shell.h>
 
 /**
- * Send a job to the foreground. Returns a 1 if the job was successfully
- * sent to the foreground, and a -1 if there was an error
+ * Send a job to the foreground. Returns a 0 if the job was successfully
+ * sent to the foreground, and a 1 if there was an error
  *
  * @args nargs Number of arguments passed to the function
  * @args args The arguments passed to the function
